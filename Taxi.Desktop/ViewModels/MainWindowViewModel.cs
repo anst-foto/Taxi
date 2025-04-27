187f28bf1c0261c781060221a225b2fd373e58c4
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-27 15:04:14 +0300

    Init

---
+++ Taxi.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Taxi.Desktop.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Taxi.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Taxi.Desktop.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
